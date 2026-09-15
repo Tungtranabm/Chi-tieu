ab5a67668fb0a86f2cb4a05cc80640388f18dec5
Tungtranabm <tungtran.abm@gmail.com>
2026-09-16 00:06:38 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,7 @@
 //
 const CONFIG = {
   APP_NAME: "Sổ chi tiêu gia đình",
-  APP_VERSION: "1.10",
+  APP_VERSION: "1.11",
 
   // Dán OAuth Client ID dạng "xxxx.apps.googleusercontent.com" vào đây.
   // Lấy ở đâu: chuỗi bạn đã dán vào ô "Client ID" của app bản trước
